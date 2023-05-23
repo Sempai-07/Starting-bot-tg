c6edc0e1ff9df040b92ce05680b33bd6ca0d71d3
Sempai-07 <mausm471@gmail.com>
2023-05-23 09:02:45 +0300

    up

--- index.js
+++ index.js
@@ -1,91 +1,101 @@
 const { TelegramBot }= require('telegramsjs');
-const {gameOptions, againOptions} = require('./options');
+const { gameOptions, againOptions } = require('./options');
 const { CreateStorage } = require('database-sempai');
+const test = require('telegramsjs');
 
-const token = ''
+const bot = new TelegramBot('6074448977:AAHmrJwDDkBBpdD0AjQK-SGuSpZHbqFZNGI');
 
-const bot = new TelegramBot(token);
 const db = new CreateStorage({
-  path: "database",
-  table: ["chatId"]
-})
+  path: 'database',
+  table: ['chatId']
+});
 
 const chats = {};
 
-const startGame = async (chatId, messageId) => {
+const startGame = async (chatId) => {
     await bot.sendMessage({
       chatId: chatId,
-      text: `Сейчас я загадаю цифру от 0 до 9, а ты должен ее угадать!`
+      text: `Зараз я загадаю число від 0 до 9, а ти мусиш її вгадати!`
     });
-    const randomNumber = Math.floor(Math.random() * 10)
+    const randomNumber = Math.floor(Math.random() * 10);
     chats[chatId] = randomNumber;
+    console.log(chats);
     await bot.sendMessage({
       chatId: chatId,
-      text: 'Отгадывай',
+      text: 'Вгадай загадане число',
       replyMarkup: gameOptions
     });
 }
 
-const start = async () => {
-
-    bot.setMyCommands({
-      commands: JSON.stringify([
-        {command: '/start', description: 'Начальное приветствие'},
-        {command: '/info', description: 'Получить информацию о пользователе'},
-        {command: '/game', description: 'Игра угадай цифру'}
+bot.on('ready', (client) => {
+  client.setCommands({
+    commands: JSON.stringify([
+      {command: '/start', description: 'Початкове привітання'},
+      {command: '/info', description: 'Отримати інформацію про користувача'},
+      {command: '/game', description: 'Гра вгадай число'}
       ])
-    })
+  })
+  console.log(`Бот запустився @${client.username}`);
+})
 
-    bot.on('privateMessageCreate', async msg => {
-        const text = msg.text;
-        const chatId = msg.chat.id;
-        const message_Id = msg.message_id;
-        try {
-            if (text === '/start') {
-                await db.has('chatId', chatId) ? db.set('chatId', chatId, chatId) : undefined;
-                await bot.sendSticker({
-                  chatId: chatId,
-                  sticker: 'https://tlgrm.ru/_/stickers/ea5/382/ea53826d-c192-376a-b766-e5abc535f1c9/7.webp'
-                })
-                return msg.reply(`Добро пожаловать в телеграм бота`);
-            }
-            if (text === '/info') {
-                const loss = db.get('chatId', `${chatId}_loss`) ?? 0;
-                const win = db.get('chatId', `${chatId}_win`) ?? 0;
-                return msg.reply(`Тебя зовут ${msg.from?.username ?? msg.from.first_name}, в игре у тебя правильных ответов ${win}, неправильных ${loss}`);
-            }
-            if (text === '/game') {
-                return startGame(chatId);
-            }
-            return msg.reply('Я тебя не понимаю, попробуй еще раз!)');
-        } catch (e) {
-            return msg.reply('Произошла какая то ошибочка!)');
-        }
+bot.on('message', async msg => {
+  const text = msg.text;
+  const chatId = msg.chat.id;
+  const message_Id = msg.message_id;
+  const username = msg.from.username ? `@${msg.from.username}` : msg.from.first_name;
+  try {
+    if (text === '/start') {
+      await db.has('chatId', chatId) ? db.set('chatId', chatId, chatId) : undefined;
+      await bot.sendSticker({
+        chatId: chatId,
+        sticker: 'https://tlgrm.ru/_/stickers/ea5/382/ea53826d-c192-376a-b766-e5abc535f1c9/7.webp'
+      })
+      return await msg.reply(`
+Ласкаво просимо до нашого телеграм-бота для гри у вгадування чисел! 🤖🎮
 
-    })
+Тут ви зможете насолоджуватися захопливою грою, де ваше завдання - вгадати число від 0 до 9. Удачі! 🤞 `);
+    }
+    if (text === '/info') {
+      const loss = db.get('chatId', `${chatId}_loss`) ?? 0;
+      const win = db.get('chatId', `${chatId}_win`) ?? 0;
+      return msg.reply(`Тебе звати ${username}, у грі у тебе правильних відповідей ${win}, неправильних ${loss}`);
+    }
+    if (text === '/game') {
+      return startGame(chatId);
+    }
+    return msg.reply('Я тебе не розумію, спробуй ще раз!');
+  } catch (e) {
+    return msg.reply('Відбулася якась помилка!)');
+  }
+})
 
-    bot.on('interactionCreate', async msg => {
-      msg.deferUpdate();
-        const data = msg.data;
-        const chatId = msg.message.chat.id;
-        if (data === '/again') {
-            return startGame(chatId);
-        }
-        const loss = await db.get('chatId', `${chatId}_loss`) ?? 0;
-       const win = await db.get('chatId', `${chatId}_win`) ?? 0;
-        if (data == chats[chatId]) {
-            await db.set('chatId', `${chatId}_win`, win + 1)
-            await msg.update(`Поздравляю, ты отгадал цифру ${chats[chatId]}`, {
-              replyMarkup: againOptions
-            });
-        } else {
-            await db.set('chatId', `${chatId}_loss`, loss + 1)
-            await msg.update(`К сожалению ты не угадал, бот загадал цифру ${chats[chatId]}`, {
-              replyMarkup: againOptions
-            });
-        }
-    })
-    await bot.login();
-}
+bot.on('callback_query', async msg => {
+  console.log(msg);
+  msg.deferUpdate();
+  const data = msg.data;
+  const chatId = msg.message.chat.id;
+  const username = msg.message.from?.username ? `${msg.message.from.username}` : msg.message.from.first_name;
+  if (data === '/again') {
+    return startGame(chatId);
+  }
+  const loss = await db.get('chatId', `${chatId}_loss`) ?? 0;
+  const win = await db.get('chatId', `${chatId}_win`) ?? 0;
+  if (data == chats[chatId]) {
+    if (chats[chatId] == undefined) {
+      msg.reply(`${username}, ви закінчили попередню гру, тому почніть нову`);
+      return;
+    }
+    await db.set('chatId', `${chatId}_win`, win + 1)
+    await msg.update(`Вітаю, ти відгадав число ${chats[chatId]}`, {
+      replyMarkup: againOptions
+    });
+  } else {
+    await db.set('chatId', `${chatId}_loss`, loss + 1);
+    await msg.update(`На жаль, ти не вгадав, бот загадав число ${chats[chatId]}`, {
+      replyMarkup: againOptions
+    });
+    await delete chats[chatId];
+  }
+});
 
-start()
+bot.login();
--- options.js
+++ options.js
@@ -1,19 +1,19 @@
-const { Button } = require('telegramsjs');
+const { Markup } = require('telegramsjs');
 const gameOptions = JSON.stringify({
     inline_keyboard: [
-      [new Button({ text: '1', action: '1' }).toJSON(), new Button({ text: '2', action: '2' }).toJSON(), new Button({ text: '3', action: '3' }).toJSON()],
-      [new Button({ text: '4', action: '4' }).toJSON(), new Button({ text: '5', action: '5' }).toJSON(), new Button({ text: '6', action: '6' }).toJSON()],
-      [new Button({ text: '7', action: '7' }).toJSON(), new Button({ text: '8', action: '8' }).toJSON(), new Button({ text: '9', action: '9' }).toJSON()],
-      [new Button({ text: '0', action: '0' }).toJSON()],
+      [new Markup({ text: '1', action: '1' }).toJSON(), new Markup({ text: '2', action: '2' }).toJSON(), new Markup({ text: '3', action: '3' }).toJSON()],
+      [new Markup({ text: '4', action: '4' }).toJSON(), new Markup({ text: '5', action: '5' }).toJSON(), new Markup({ text: '6', action: '6' }).toJSON()],
+      [new Markup({ text: '7', action: '7' }).toJSON(), new Markup({ text: '8', action: '8' }).toJSON(), new Markup({ text: '9', action: '9' }).toJSON()],
+      [new Markup({ text: '0', action: '0' }).toJSON()],
     ]
   })
 
 const againOptions = JSON.stringify({inline_keyboard: [
-      [new Button({ text: 'Играть еще раз', action: '/again' }).toJSON()]
+      [new Markup({ text: 'Грати ще раз', action: '/again' }).toJSON()]
       ]})
 
 module.exports = {
   gameOptions,
   againOptions,
-  Button,
+  Markup,
 };
